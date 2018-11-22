ad13b47a48071515415f6b9164af129992b8593d
alaulong <alaulong@gmail.com>
2018-11-23 04:42:24 +0800

    2018112305

--- MainControl2.0.0027/MotorAddSam.c
+++ MainControl2.0.0027/MotorAddSam.c
@@ -9,29 +9,29 @@ void resetAdd1Motor()
     
         // 加样泵电机复位
 	TRY(bOk = MotorReset(motorAddS1.nAddr));
-        if (!bOk) 
-        {
-          Set_Add1_State(Add1_Err_Transmition);
-          return;
+    if (!bOk) 
+    {
+        Set_Add1_State(Add1_Err_Transmition);
+        return;
+    }
+    
+    while  (1)//加样泵
+    {
+            //获取加样泵状态
+        TRY(bOk = MotorGetStatus(motorAddS1.nAddr, &status));
+        if (!bOk) {Set_Add1_State(Add1_Err_Transmition);return;}
+        if (status == 0x0000) break;
+            //样品泵原点失败
+        else if (status >0x8000)  {Set_Add1_State(Add1_Err_Origin);return;}
+            //是否取消
+        if (IS_Add1_Cancel_FLAG()){
+            TRY(MotorCancel(motorAddS1.nAddr));
+            Reset_Add1_Cancel_FLAG();
+            return;
         }
-        
-        while  (1)//加样泵
-        {
-                //获取加样泵状态
-                TRY(bOk = MotorGetStatus(motorAddS1.nAddr, &status));
-		if (!bOk) {Set_Add1_State(Add1_Err_Transmition);return;}
-                if (status == 0x0000) break;
-                //样品泵原点失败
-		else if (status >0x8000)  {Set_Add1_State(Add1_Err_Origin);return;}
-                //是否取消
-                if (IS_Add1_Cancel_FLAG()){
-                  TRY(MotorCancel(motorAddS1.nAddr));
-                  Reset_Add1_Cancel_FLAG();
-                  return;
-                }
-                OSTimeDly(100);
-        }
-        Set_Add1_State(Add1_Idle);
+        OSTimeDly(100);
+    }
+    Set_Add1_State(Add1_Idle);
 }
 ///
 ///----------------------------------------------------------------------------
@@ -42,110 +42,30 @@ void Add1SetPos(void)
 	u8	bOk = 1,Add1FLAG;
 	u16	status;
 	s32	Pos;
-        u16     Per;
-        u16     Min,Max;
-        
-        GetAdd1Pos(&Add1FLAG,&Pos,&Per);
-        if (Add1FLAG == 0x81 || Add1FLAG == 0x01)//判断样品泵目标位置是否有效
+    
+    GetAdd1Pos(&Add1FLAG,&Pos);
+    if (Add1FLAG == 0x81 || Add1FLAG == 0x01)//判断add1motor目标位置是否有效
+    {
+        TRY(bOk = MotorMoveTo(motorAddS1.nAddr, Pos));
+        if (!bOk) {Set_Add1_State(Add1_Err_Transmition);return;}
+        while  (1)//样品泵
         {
-            TRY(bOk = MotorMoveTo(motorAddS1.nAddr, Pos));
+            //获取样品泵状态
+            TRY(bOk = MotorGetStatus(motorAddS1.nAddr, &status));
             if (!bOk) {Set_Add1_State(Add1_Err_Transmition);return;}
-            while  (1)//样品泵
-            {
-                //获取样品泵状态
-                TRY(bOk = MotorGetStatus(motorAddS1.nAddr, &status));
-		if (!bOk) {Set_Add1_State(Add1_Err_Transmition);return;}
-                if (status == 0x0000) break;
-                //样品泵原点失败
-		else if (status >0x8000)  {Set_Syringe_State(Syringe_Err_Transmition_S);return;}
-                //是否取消
-                if (IS_Syringe_Cancel_FLAG()){
-                  TRY(MotorCancel(MotorSyringSample.nAddr));
-                  Reset_Syringe_Cancel_FLAG();
-                  return;
-                }
-                OSTimeDly(10);
+            if (status == 0x0000) break;
+            //样品泵原点失败
+            else if (status >0x8000)  {Set_Add1_State(Add1_Err_Transmition);return;}
+            //是否取消
+            if (IS_Add1_Cancel_FLAG()){
+                TRY(MotorCancel(motorAddS1.nAddr));
+                Reset_Add1_Cancel_FLAG();
+                return;
             }
+            OSTimeDly(10);
         }
-        else if (SyringeFLAG == 0x82 || SyringeFLAG == 0x02)
-        {
-            Min = (u16)(((float)MotorSyringReagent.nSpeedMin/100.0)*(float)Per);
-            Max = (u16)(((float)MotorSyringReagent.nSpeedMax/100.0)*(float)Per);
-            TRY(bOk = MotorSetHighSpeed(MotorSyringReagent.nAddr, Min , Max ,MotorSyringReagent.nSpeedStep));
-            if (!bOk) {Set_Syringe_State(Syringe_Err_Transmition_R);return;}
-            TRY(bOk = MotorMoveTo(MotorSyringReagent.nAddr, Pos));
-            if (!bOk) {Set_Syringe_State(Syringe_Err_Transmition_R);return;}
-            while  (1)//试剂泵
-            {
-                //获取试剂泵状态
-                TRY(bOk = MotorGetStatus(MotorSyringReagent.nAddr, &status));
-		if (!bOk) {Set_Syringe_State(Syringe_Err_Transmition_R);return;}
-                if (status == 0x0000) break;
-                //试剂泵原点失败
-		else if (status >0x8000)  {Set_Syringe_State(Syringe_Err_Transmition_R);return;}
-                //是否取消
-                if (IS_Syringe_Cancel_FLAG()){
-                  TRY(MotorCancel(MotorSyringReagent.nAddr));
-                  Reset_Syringe_Cancel_FLAG();
-                  return;
-                }
-                OSTimeDly(10);
-            }
-        }
-        else if (SyringeFLAG==0x03)
-        {
-            TRY(bOk = MotorMoveTo(MotorSyringSample.nAddr, Pos));
-            if (!bOk) {Set_Syringe_State(Syringe_Err_Transmition_S);return;}
-            TRY(bOk = MotorMoveTo(MotorSyringReagent.nAddr, Pos));
-            if (!bOk) {Set_Syringe_State(Syringe_Err_Transmition_R);return;}
-            
-            while  (1)//样品泵
-            {
-                //获取样品泵状态
-                TRY(bOk = MotorGetStatus(MotorSyringSample.nAddr, &status));
-		if (!bOk) {Set_Syringe_State(Syringe_Err_Transmition_S);return;}
-                if (status == 0x0000) break;
-                //样品泵原点失败
-		else if (status >0x8000)  {Set_Syringe_State(Syringe_Err_Transmition_S);return;}
-                //是否取消
-                if (IS_Syringe_Cancel_FLAG()){
-                  TRY(MotorCancel(MotorSyringSample.nAddr));
-                  Reset_Syringe_Cancel_FLAG();
-                  return;
-                }
-                OSTimeDly(10);
-            }
-            
-            while  (1)//试剂泵
-            {
-                //获取试剂泵状态
-                TRY(bOk = MotorGetStatus(MotorSyringReagent.nAddr, &status));
-		if (!bOk) {Set_Syringe_State(Syringe_Err_Transmition_R);return;}
-                if (status == 0x0000) break;
-                //试剂泵原点失败
-		else if (status >0x8000)  {Set_Syringe_State(Syringe_Err_Transmition_R);return;}
-                //是否取消
-                if (IS_Syringe_Cancel_FLAG()){
-                  TRY(MotorCancel(MotorSyringReagent.nAddr));
-                  Reset_Syringe_Cancel_FLAG();
-                  return;
-                }
-                OSTimeDly(10);
-            } 
-        }
-        else if (SyringeFLAG ==0x04)
-        {
-            TRY(bOk = MotorSetValve(MotorSyringSample.nAddr, Pos));
-            if (!bOk) {Set_Syringe_State(Syringe_Err_Transmition_S);return;}
-        }
-        else if (SyringeFLAG==0x05)
-        {
-            TRY(bOk = MotorSetValve(MotorSyringReagent.nAddr, Pos));
-            if (!bOk) {Set_Syringe_State(Syringe_Err_Transmition_S);return;}
-        }
-        
-        
-        Set_Syringe_State(Syringe_Idle);
+    }
+    Set_Add1_State(Add1_Idle);
 }
 
 
@@ -182,7 +102,7 @@ void HandleAdd1(void)
     }
     if (IS_Add1_MOVE_FLAG())
     {
-        Set_Add1_State(Syringe_Busy);
+        Set_Add1_State(Add1_Busy);
         RESET_Add1_MOVE_FLAG();
         Add1SetPos();
     }
@@ -204,7 +124,7 @@ void Reset_Add1_Cancel_FLAG(void)
 //泵复位标志
 u8 Add1_R_FLAG=0;
 //泵复位标志有效
-u8 Add1_R_FLAG(void)
+u8 GET_Add1_R_FLAG(void)
 {
     return Add1_R_FLAG;
 }
@@ -235,22 +155,20 @@ void RESET_Add1_MOVE_FLAG(void)
 {
     Add1_MOVE_FLAG=0;
 }
-u16 Add1_Per;
+
 //泵的标志
 u8 Add1_FLAG;
 //采样泵坐标，试剂泵坐标
 s32 Add1_POS;
 //获取标志，坐标
-void GetSyringePos(u8* SyringeFLAG,s32* Pos, u16 *Per)
+void GetAdd1Pos(u8* Add1FLAG,s32* Pos)
 {
-    *SyringeFLAG=Add1_FLAG;
+    *Add1FLAG=Add1_FLAG;
     *Pos=Add1_POS;
-    *Per=Add1_Per;
 }
 //设定泵标志，坐标
-void SetAdd1Pos(u8 SyringeFLAG,s32 Pos,u16 Per)
+void SetAdd1Pos(u8 Add1FLAG,s32 Pos)
 {
-    Add1_FLAG=SyringeFLAG;
+    Add1_FLAG=Add1FLAG;
     Add1_POS=Pos;
-    Add1_Per = Per;
 }
